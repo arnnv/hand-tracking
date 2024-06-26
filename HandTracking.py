31995fd7755c5d04e42fe1edcef414e2b742007f
Arnav <arnnv.g@gmail.com>
2024-06-26 17:26:11 +0530

    add hand detection

--- HandTracking.py
+++ HandTracking.py
@@ -2,13 +2,42 @@ import cv2
 import mediapipe as mp
 import time
 
+# Initialize the video capture object
 cap = cv2.VideoCapture(0)
 
+# Initialize the MediaPipe Hands solution
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
 
 while True:
     success, img = cap.read()
+    if not success:
+        break
+
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    # print(results.multi_hand_landmarks)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, (128, 0, 128), 2)
 
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+
+    # Break the loop when 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the video capture object and close all OpenCV windows
+cap.release()
+cv2.destroyAllWindows()
